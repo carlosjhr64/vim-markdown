88e99ebee4d53df78edf283cd221f4f9e03549a6
carlosjhr64 <carlosjhr64@gmail.com>
2021-07-12 10:44:49 -0700

    minimal syntaxing (not our job here)

--- lib/vim-markdown.rb
+++ lib/vim-markdown.rb
@@ -1,6 +1,6 @@
 # VimMarkdown is loaded into VIM when user opens a markdown file.
 module VimMarkdown
-  VERSION = '1.0.210220'
+  VERSION = '2.0.210712'
 
   # Note: Highly overloaded
   def self.metadata(key=nil, value=nil)
--- lib/vim-markdown/syntax.rb
+++ lib/vim-markdown/syntax.rb
@@ -1,53 +1,18 @@
 module VimMarkdown
 module Syntax
   def self.after_syntax
-    # Functions
-    [
-      %q{
-        function VimMarkdownFoldText()
-          if (v:foldstart == 1) && (getline(1) =~ '\_^---')
-            return "--- MetaData(zo/zc to view/close) "
-          end
-          let n = v:foldend - v:foldstart + 1
-          return "+-- ".n." lines: "
-        endfunction
-      },
-    ].each{ |func| VIM.command func.strip }
-    # Commands
-    [
-      # Conceals 
-      %q{
-        syn region markdownCode
-        matchgroup=markdownCodeDelimiter
-        start="`" end="`"
-        keepend contains=markdownLineStart
-        concealends
-      },
-      %q{
-        syn region markdownLinkText
-        matchgroup=markdownLinkTextDelimiter
-        start="!\=\[\%(\_[^]]*]\%( \=[[(]\)\)\@=" end="\]\%( \=[[(]\)\@="
-        nextgroup=markdownLink,markdownId skipwhite
-        contains=@markdownInline,markdownLineStart
-        concealends
-      },
-      %q{
-        syn region markdownLink
-        matchgroup=markdownLinkDelimiter
-        start="(" end=")"
-        contains=markdownUrl keepend contained
-        conceal cchar=⁺
-      },
-      'syn match VimMarkdownHelpHyperTextJump "|[^"*| ]\\+|" contains=VimMarkdownHelpBar',
-      'syn match VimMarkdownHelpBar contained "|" conceal',
-      'hi def link VimMarkdownHelpHyperTextJump Identifier',
-      'hi clear Conceal',
-      'setlocal conceallevel=2',
-      # Folds
-      'syn region VimMarkdownMetaDataFold start="\%^---" end="---" keepend transparent fold',
-      'setlocal foldmethod=syntax',
-      'setlocal foldtext=VimMarkdownFoldText()',
-    ].each{ |cmd| VIM.command cmd.strip.gsub(/\s+/, ' ') }
+    VIM.command %q{
+      function VimMarkdownFoldText()
+        if (v:foldstart == 1) && (getline(1) =~ '\_^---')
+          return "--- MetaData(zo/zc to view/close) "
+        end
+        let n = v:foldend - v:foldstart + 1
+        return "+-- ".n." lines: "
+      endfunction
+      syn region vimMarkdownMetaDataFold start="\%^---" end="---" keepend transparent fold
+      setlocal foldmethod=syntax
+      setlocal foldtext=VimMarkdownFoldText()
+    }
   end
 end
 end
